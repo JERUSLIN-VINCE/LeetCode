7d8d4f4d6b8a25a6f72125e4f66237f3889f12eb
JERUSLIN-VINCE <jeruslinvince@gmail.com>
2025-03-13 22:10:57 +0530

    Update and rename maddy.txt to Movezero.java

---
+++ Movezero.java
@@ -0,0 +1,26 @@
+(* problem statement:
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+
+solution
+Language used:JAVA
+*)
+      class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+         int id=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[id++]=nums[i];
+            }
+        } while(id<nums.length){
+            nums[id]=0;
+              id++;}
+    }
+  public static void main(String[] args){
+    int[] nums={0,1,0,3,12};
+    moveZeros(nums);
+    System.out.print(Arrays.toString(nums))
+  }
+
+
